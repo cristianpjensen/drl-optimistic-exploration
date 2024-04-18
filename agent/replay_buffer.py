d22967d77c49c70bd5fdeb01236f632d2d6564e3
Cristian Perez Jensen <cristianpjensen@gmail.com>
2024-04-18 15:57:17 +0200

    fix(replay buffer): revert terminal change

--- agent/replay_buffer.py
+++ agent/replay_buffer.py
@@ -1,7 +1,6 @@
 from typing import Tuple
 
 import gymnasium as gym
-import numpy as np
 import torch
 
 
@@ -117,125 +116,12 @@ class ReplayBuffer:
         # Use the frame stack by sampling the last frame and then stacking the previous frames.
         # Also, make sure not to sample last index, since then we won't know the next state.
         indices = torch.multinomial(self.sample_prob[:-2].float(), self.batch_size)
-        stacked_indices = indices.reshape(-1, 1) + torch.arange(-(self.frame_stack - 1), 1)
+        stacked_indices = indices.reshape(-1, 1) + torch.arange(-(self.frame_stack - 1), 1).to(self.device)
 
-        return self.frames[stacked_indices], self.actions[indices], self.rewards[indices], self.frames[stacked_indices+1], self.terminals[indices+1]
+        return self.frames[stacked_indices], self.actions[indices], self.rewards[indices], self.frames[stacked_indices+1], self.terminals[indices]
 
     def __len__(self):
         return min(self.entries, self.max_size)
 
     def is_ready(self):
         return len(self) >= self.min_size
-
-
-# class ReplayBuffer:
-#     """A simple replay buffer that stores transitions with a frame stacking mechanisms.
-
-#     Args:
-#         observation_space: The observation space of the environment.
-#         action_space: The action space of the environment.
-#         min_size: Minimum size of the buffer before we start sampling. Default: 50_000
-#             (https://www.nature.com/articles/nature14236).
-#         max_size: Maximum size of the buffer. Default: 1_000_000
-#             (https://www.nature.com/articles/nature14236).
-#         batch_size: Number of samples to return when sampling. Default: 32
-#             (https://www.nature.com/articles/nature14236).
-#         frame_stack: Number of frames to stack when sampling. Default: 1, set to 4 for Atari games
-#            (https://www.nature.com/articles/nature14236).
-#         device: CUDA if available, otherwise CPU.
-
-#     """
-
-#     def __init__(
-#         self,
-#         observation_space: gym.Space,
-#         action_space: gym.Space,
-#         min_size: int = 50_000,
-#         max_size: int = 1_000_000,
-#         batch_size: int = 32,
-#         frame_stack: int = 1,
-#         device: torch.device = torch.device("cpu"),
-#     ):
-#         self.min_size = min_size
-#         self.max_size = max_size
-#         self.batch_size = batch_size
-#         self.frame_stack = frame_stack
-#         self.device = device
-
-#         # Need to sample observation and action to get the correct pytorch types
-#         # obs_sample = torch.tensor(observation_space.sample())
-#         # act_sample = torch.tensor(action_space.sample())
-
-#         # self.states = torch.zeros((max_size, *observation_space.shape), dtype=obs_sample.dtype, device=device)
-#         # self.actions = torch.zeros((max_size, *action_space.shape), dtype=act_sample.dtype, device=device)
-#         # self.rewards = torch.zeros((max_size, 1), dtype=torch.float32, device=device)
-#         # self.terminals = torch.zeros((max_size, 1), dtype=torch.bool, device=device)
-
-#         # Keep track of which index was last added, since we do not want to sample any frames that
-#         # contain `last_added`
-#         self.last_added = 0
-#         self.entries = 0
-
-#     def push(self, state, action, reward, terminal):
-#         # Implemented as deque
-#         index = self.entries % self.max_size
-
-#         self.states[index] = torch.tensor(state)
-#         self.actions[index] = torch.tensor(action)
-#         self.rewards[index] = torch.tensor(reward)
-#         self.terminals[index] = torch.tensor(terminal)
-
-#         self.entries += 1
-#         self.last_added = index
-
-#     def sample_batch(self) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-#         # Use the frame stack by sampling the last frame and then stacking the previous frames
-
-#         # Sample random indices
-#         indices = torch.randint(
-#             self.frame_stack - 1,
-#             min(self.entries, self.max_size - 1),
-#             (self.batch_size,),
-#         )
-
-#         # Do not allow sampling between `last_added+1` (inclusive) and `last_added+frame_stack` (exclusive).
-#         # So, we do not sample states that contain the last added state, which would overlap between episodes.
-#         # We do this by sampling between `frame_stack - 1` (to allow stacking) and `size - (frame_stack+1)` (to
-#         # allow shifting after sampling, such that we avoid the last added frame).
-#         indices = torch.randint(
-#             self.frame_stack - 1,
-#             min(self.entries, min(self.entries, self.max_size - (self.frame_stack+1))),
-#             (self.batch_size,),
-#         )
-#         after_last_added = indices > self.last_added
-#         indices = torch.where(after_last_added, indices + self.frame_stack, indices)
-
-#         # Stack frames
-#         stacked_indices = (indices.reshape(-1, 1) + torch.arange(-(self.frame_stack - 1), 1)).flatten()
-#         state = self.states[stacked_indices]
-#         state = state.reshape(self.batch_size, self.frame_stack, *state.shape[1:])
-
-#         # Next state with stacked frames aswell but shifted one
-#         next_state = self.states[stacked_indices + 1]
-#         next_state = next_state.reshape(self.batch_size, self.frame_stack, *next_state.shape[1:])
-
-#         # Action and rewards
-#         action = self.actions[indices]
-#         reward = self.rewards[indices]
-
-#         # Terminal if next state is terminal
-#         terminal = self.terminals[indices + 1]
-
-#         # Check for overlapping episodes by checking whether any of the frames of the state are terminal
-#         discard = self.terminals[stacked_indices]
-#         discard = discard.reshape(self.batch_size, self.frame_stack).any(dim=1)
-
-#         keep = discard.logical_not()
-
-#         return state[keep], action[keep], reward[keep], next_state[keep], terminal[keep]
-
-#     def __len__(self):
-#         return min(self.entries, self.max_size)
-
-#     def is_ready(self):
-#         return len(self) >= self.min_size
