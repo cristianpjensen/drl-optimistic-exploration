0c79098c763c0fec52da47f66e0978225f5db0a1
scestola <scestola@student.ethz.ch>
2024-04-24 04:57:55 +0200

    iqn implementation (please check it because I think it could be improved)

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,9 +1,11 @@
 from agent.agent import RandomAgent
 from agent.dqn import AtariDQNAgent
+from agent.iqn import AtariIQNAgent
 from agent.tabular_q_learning import TabularQLearningAgent
 
 AGENTS = {
     "random": RandomAgent,
     "atari-dqn": AtariDQNAgent,
+    "atari-iqn": AtariIQNAgent,
     "tabular-q-learning": TabularQLearningAgent,
 }
